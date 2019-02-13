374c203c2acb2e3d2c248ca84276e0eea34b1d80
DESKTOP-T3NFU2U\seye2 <seye2@nate.com>
2019-02-13 14:07:10 +0900

    error boundary-hoc

---
+++ boundary-hoc/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+const BoundaryHOC = ProtectedComponent => {
+    return class Boundary extends Component {
+      state = {
+        hasError: false
+      };
+      componentDidCatch = () => {
+        this.setState({
+          hasError: true
+        });
+      };
+      render() {
+        const { hasError } = this.state;
+        if (hasError) {
+          return <ErrorFallback />;
+        } else {
+          return <ProtectedComponent />;
+        }
+      }
+    };
+}
+
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
+const PErrorMaker = BoundaryHOC(ErrorMaker);
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
+
+const PPortals = BoundaryHOC(Portals);
+
+const Message = () => "Just touched it!";
+
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
+
+const ErrorFallback = () => "Sorry something went wrong";
+
+class App extends Component {
+  render() {
+    return (
+        <Fragment>
+          <ReturnTypes />
+          <PPortals />
+          <PErrorMaker />
+        </Fragment>
+    );
+  }
+}
+
+export default BoundaryHOC(App);
