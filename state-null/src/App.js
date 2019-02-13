7ceb2a716ad1c1f10ea053c6c9b9353c9ec669f0
DESKTOP-T3NFU2U\seye2 <seye2@nate.com>
2019-02-13 14:31:28 +0900

    state null

---
+++ state-null/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+const MAX_PIZZAS = 20;
+
+const eatPizaa = (state, props) => {
+  const { pizzas } = state;
+  if (pizzas < MAX_PIZZAS) {
+    return {
+      pizzas: pizzas + 1
+    };
+  } else {
+    return null;
+  }
+};
+
+class Controlled extends Component {
+  state = {
+    pizzas: 0
+  };
+  render() {
+    const { pizzas } = this.state;
+    return (
+        <button onClick={this._handleClick}>{`I have eaten ${pizzas} ${
+            pizzas === 1 ? "pizza" : "pizzas"
+            }`}</button>
+    );
+  }
+  _handleClick = () => {
+    this.setState(eatPizaa);
+  };
+}
+
+class App extends Component {
+  render() {
+    return <Controlled />;
+  }
+}
+
+export default App;
