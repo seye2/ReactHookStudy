6a23fb053f97d9a377c231e5138890de60a180d9
DESKTOP-T3NFU2U\seye2 <seye2@nate.com>
2019-02-14 19:00:16 +0900

    add example use-state

---
+++ use-state/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Axios from "axios";
+//import "./styles.css";
+
+function useInput(defaultValue) {
+  const [value, setValue] = useState(defaultValue);
+
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+    setValue(value);
+  };
+
+  return { value, onChange };
+}
+
+function useFetch(url) {
+  const [payload, setPayload] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const callUrl = async () => {
+    try {
+      const { data } = await Axios.get(url);
+      setPayload(data);
+    } catch {
+      setError("😭");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    callUrl();
+  }, []);
+
+  return { payload, loading, error };
+}
+
+function App() {
+  const name = useInput("");
+  const { payload, loading, error } = useFetch("https://aws.random.cat/meow");
+  return (
+    <div className="App">
+      <h1>Use Hooks</h1>
+      <br />
+      <input {...name} placeholder="Whats your name" />
+      <br />
+      {loading && <span>loading your cat</span>}
+      {!loading && error && <span>{error}</span>}
+      {!loading && payload && <img src={payload.file} width="250" />}
+    </div>
+  );
+}
+
+export default App;
