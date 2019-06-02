a76c4118fa18cb708fbead9c8bf49a71b0dc8c77
남동훈 <seye1479@gmail.com>
2019-06-02 22:30:59 +0900

    featureV1 fix1

--- use-state/src/App.js
+++ use-state/src/App.js
@@ -5,7 +5,7 @@ import Axios from "axios";
 
 function useInput(defaultValue) {
   const [value, setValue] = useState(defaultValue);
-console.log(value);
+console.log('featureV1 fix1');
   const onChange = e => {
     const {
       target: { value }
