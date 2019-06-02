690a7752f4cc5d7fd20b68c219cbff6db9291faf
남동훈 <seye1479@gmail.com>
2019-06-02 22:23:19 +0900

    featureV2 fix1

--- use-state/src/App.js
+++ use-state/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import Axios from "axios";
-//import "./styles.css";
 
 function useInput(defaultValue) {
   const [value, setValue] = useState(defaultValue);
